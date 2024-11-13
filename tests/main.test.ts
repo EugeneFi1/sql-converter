e6e4e5c6c2c33560cd2f3407b3fe9f9f6fe91c82
EugeneFi1 <eugenefil.ua@gmail.com>
2024-11-13 21:36:53 +0200

    improve tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -7,18 +7,22 @@ import { TABLE_METADATA } from "../mock-data/tables";
  * [<intermediate transaction language string>, <expected SQL-query>]
  */
 const QUERIES_TO_TEST = [
+  // test SELECT from one table
   [
     "SELECT first_name, last_name FROM users WHERE id > 1 ORDER BY first_name",
     `SELECT "first_name", "last_name" FROM "users" WHERE "id" > 1 ORDER BY "first_name" ASC`,
   ],
+  // test with avg()
   [
     "SELECT departments.name, avg(salary) FROM users GROUP BY departments.name",
     'SELECT "departments"."name" AS "departments.name", AVG("users"."salary") AS "users.salary" FROM "users" LEFT JOIN "users_departments" ON "users"."id" = "users_departments"."users_id" LEFT JOIN "departments" AS "departments" ON "users_departments"."departments_id" = "departments"."id" GROUP BY "departments"."name"',
   ],
+  // test SELECT from more than one table
   [
     "SELECT id, title, owner.id, watchers.id FROM tasks",
     'SELECT "tasks"."id", "tasks"."title", "owner"."id" AS "owner.id", "watchers"."id" AS "watchers.id" FROM "tasks" LEFT JOIN "users" AS "owner" ON "tasks"."owner_id" = "owner"."id" LEFT JOIN "tasks_watchers" ON "tasks"."id" = "tasks_watchers"."tasks_id" LEFT JOIN "users" AS "watchers" ON "tasks_watchers"."watchers_id" = "watchers"."id"',
   ],
+  // test custom JOIN (RIGHT JOIN)
   [
     "SELECT id, title, owner.id AS 'owner.id', watchers.id FROM tasks RIGHT JOIN users AS owner ON tasks.owner_id = owner.id",
     'SELECT "tasks"."id", "tasks"."title", "owner"."id" AS "owner.id", "watchers"."id" AS "watchers.id" FROM "tasks" RIGHT JOIN "users" AS "owner" ON "tasks"."owner_id" = "owner"."id" LEFT JOIN "tasks_watchers" ON "tasks"."id" = "tasks_watchers"."tasks_id" LEFT JOIN "users" AS "watchers" ON "tasks_watchers"."watchers_id" = "watchers"."id"',
